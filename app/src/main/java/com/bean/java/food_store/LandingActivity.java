dd6818ae07b7fab07ffa1dd7aa0fd74e34d008b4
moses.mathebula16@gmail.com <Mouzie1621>
2017-08-24 13:56:51 +0200

    Initial commit - Repository creation and progress saving.

---
+++ app/src/main/java/com/bean/java/food_store/LandingActivity.java
@@ -0,0 +1,19 @@
+package com.bean.java.food_store;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class LandingActivity extends AppCompatActivity {
+
+    Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_landing);
+
+        //Calling activity that will handle registration
+        
+    }
+}
